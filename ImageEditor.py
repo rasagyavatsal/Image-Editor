187790e9aecf125867d030aa730d84c2ebd1eae4
Rasagya Vatsal <132339712+rasagyavatsal@users.noreply.github.com>
2024-10-03 18:39:20 +0530

    modified:   ImageEditor.py

--- ImageEditor.py
+++ ImageEditor.py
@@ -10,23 +10,9 @@ def PNGtoGIF():
     img = Image.open(inputImg)
     img.save('output_image.gif')
 
-def PNGtoTIFF():
-    inputImg = input("Enter img path: ")
-    img = Image.open(inputImg)
-    img.save('output_image.tiff', format='TIFF')
-
-def PNGtoBMP():
-    inputImg = input("Enter img path: ")
-    img = Image.open(inputImg)
-    img.save('output_image.bmp', format='BMP')
-
 usrInput=input("Enter conversion type: ")
 
 if usrInput=="JPEG":
     PNGtoJPEG()
 elif usrInput=="GIF":
     PNGtoGIF()
-elif usrInput=="TIFF":
-    PNGtoTIFF()
-elif usrInput=="BMP":
-    PNGtoBMP()
